a5f089ac6adcda57945f9a78873a758e606c8753
L0rd <amodogbe@gmail.com>
2019-11-06 16:39:05 +0100

    ajout champs et méthode findView (@id) suppression du casting pour les methodes findView après l'apparition du message d'erreur "Casting findViewById(...) to '' is redundant. Résolution: https://stackoverflow.com/questions/44902651/no-need-to-cast-the-result-of-findviewbyid

--- app/src/main/java/amoni/ayyub/topquiz/MainActivity.java
+++ app/src/main/java/amoni/ayyub/topquiz/MainActivity.java
@@ -3,12 +3,25 @@ package amoni.ayyub.topquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWelcomeText;
+    private EditText mNameInput;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWelcomeText = findViewById(R.id.activity_main_welcome_txt);
+        mNameInput = findViewById(R.id.activity_main_txt_input);
+        mButton = findViewById(R.id.activity_main_play_btn);
     }
+
+
 }
