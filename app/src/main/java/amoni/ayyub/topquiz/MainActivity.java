bbe07450c12370d29fc8aa8b6201c58bca6d95e1
L0rd <amodogbe@gmail.com>
2019-11-06 17:35:12 +0100

    Gerer les actions User

--- app/src/main/java/amoni/ayyub/topquiz/MainActivity.java
+++ app/src/main/java/amoni/ayyub/topquiz/MainActivity.java
@@ -3,11 +3,17 @@ package amoni.ayyub.topquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Création des champs (attributs) requis
+     */
 
     private TextView mWelcomeText;
     private EditText mNameInput;
@@ -21,6 +27,35 @@ public class MainActivity extends AppCompatActivity {
         mWelcomeText = findViewById(R.id.activity_main_welcome_txt);
         mNameInput = findViewById(R.id.activity_main_txt_input);
         mButton = findViewById(R.id.activity_main_play_btn);
+
+        //Désactiver le bouton de l'interface
+        mButton.setEnabled(false);
+
+        //Notifié lorsque l'utilisateur commence à saisir du texte dans le champ EditText
+        mNameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mButton.setEnabled(s.toString().length() != 3);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //Détecter que l'utilisateur a cliqué sur le bouton
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 
